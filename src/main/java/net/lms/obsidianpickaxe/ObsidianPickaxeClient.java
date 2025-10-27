24bed7b3a16e27496dcd5e6ff7601d0f66f1b2bf
Lennart Sahler <lennart.sahler@outlook.de>
2025-10-27 13:31:39 +0100

    Initial commit

---
+++ src/main/java/net/lms/obsidianpickaxe/ObsidianPickaxeClient.java
@@ -0,0 +1,10 @@
+package net.lms.obsidianpickaxe;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ObsidianPickaxeClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
